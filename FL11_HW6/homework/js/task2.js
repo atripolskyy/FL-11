4d06c24cb02e9db41c120f0acef6168a2fd6effb
Oleksii Trypolskyi <atripolskyy@gmail.com>
2019-07-03 08:17:32 +0300

    second commit

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,15 @@
 // Your code goes here
+let a = +prompt('Type first triangle side');
+
+let b = +prompt('Type second triangle side');
+
+let c = +prompt('Type third triangle side');
+
+if (a + b > c && b + c > a && c + a > b) {
+  console.log('Triangle exist');
+  if (a === b && b === c && a === c) {
+    console.log('Eequivalent triangle');
+  }
+} else {
+  console.log('Triangle doesn\'t exist');
+}
