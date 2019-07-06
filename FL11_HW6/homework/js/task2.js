b399faf78e7c982d173b286c49126b915113252c
Oleksii Trypolskyi <atripolskyy@gmail.com>
2019-07-06 20:25:26 +0300

    2 task commit

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,15 +1,17 @@
-// Your code goes here
-let a = +prompt('Type first triangle side');
-
-let b = +prompt('Type second triangle side');
-
-let c = +prompt('Type third triangle side');
+let a = +prompt('Type first triangle side'),
+    b = +prompt('Type second triangle side'),
+    c = +prompt('Type third triangle side');
 
 if (a + b > c && b + c > a && c + a > b) {
-  console.log('Triangle exist');
-  if (a === b && b === c && a === c) {
-    console.log('Eequivalent triangle');
+  if (a === b && b === c) {
+    console.log( 'Eequivalent triangle' );
+  } else {
+    if ( a === b || b === c || a === c) {
+      console.log( 'Isosceles triangle' );
+    } else {
+      console.log( 'Normal triangle' );
+    }
   }
 } else {
-  console.log('Triangle doesn\'t exist');
+  console.log( 'Triangle doesn\'t exist' );
 }
