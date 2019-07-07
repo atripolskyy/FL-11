514913c09e113e5ff07d0bc577fe7404140b89aa
Oleksii Trypolskyi <atripolskyy@gmail.com>
2019-07-07 16:03:11 +0300

    another commit

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,20 @@
-// Your code goes here
+let a1 = +prompt('Type coordinate for a1'),
+    a2 = +prompt('Type coordinate for a2'),
+    b1 = +prompt('Type coordinate for b1'),
+    b2 = +prompt('Type coordinate for b2'),
+    c1 = +prompt('Type coordinate for c1'),
+    c2 = +prompt('Type coordinate for c2');
+
+let digits = 10;
+
+let formula = +((b2 - a2)*c1/(b1 - a1) + a2 - (b2 - a2)*a1/(b1 - a1)).toFixed( digits );
+
+if ( c2 === formula ) {
+  if ( c1 === (a1 + b1)/2 && c2 === (a2 + b2)/2 ) {
+    console.log( true );
+  } else {
+    console.log( false );
+  }
+} else {
+  console.log( false );
+}
